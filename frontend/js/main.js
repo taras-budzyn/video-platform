fdba55256e8cc9a8152a5b8032c286379586a6e5
taras.budzyn <taras.budzyn@gmail.com>
2021-09-03 15:57:06 +0300

    added basic UI logic presentation

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,37 @@
 const VideoApp = {
     data() {
         return {
-            burgerMenuOpen: false
+            burgerMenuOpen: false,
+            searchQuery: '',
+            categories: [],
+            videoList: [...Array(6).keys()],
+            currentPage: 'Home',
+            showNotification: false,
+            error: ''
         }
+    },
+    mounted() {
+        ApiService.getYoutubeCategories();
+        ApiService.getPopularVideos();
     },
     methods: {
         openCloseMenu() {
             this.burgerMenuOpen = !this.burgerMenuOpen;
         },
+        hideNotification() {
+            this.showNotification = false;
+            this.error = '';
+        },
         search() {
-            console.log('Search!');
+            console.log('Search!', this.searchQuery);
+            this.currentPage = 'Search results for:' + this.searchQuery;
+
+        },
+        chooseCategory(category) {
+            console.log('Category!', category);
+            this.currentPage = 'Category:' + category;
+            this.videoList = [...Array(6).keys()];
+            this.searchQuery = '';
         }
     }
 }
---
+++ frontend/js/services/api.js
@@ -0,0 +1,14 @@
+const ApiService = {
+    searchVideos: function (searchQuery) {
+        console.log('searchVideos');
+    },
+    getVideoOfCategory: function() {
+        console.log('getVideoOfCategory');
+    },
+    getYoutubeCategories: function() {
+        console.log('getYoutubeCategories');
+    },
+    getPopularVideos: function() {
+        console.log('getPopularVideos');
+    }
+}
